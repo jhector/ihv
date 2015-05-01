6f9d9b460b050229d10fd82dad02120400cc51e2
Julian Hector <julian.r.hector@gmail.com>
2015-05-01 10:10:54 +0200

    Added database interaction for hooks

--- main.cpp
+++ main.cpp
@@ -13,65 +13,130 @@
 #include <set>
 #include <map>
 
+#include "db.h"
+#include "mysql.h"
+
 /* ===================================================================== */
 /* Names of malloc and free */
 /* ===================================================================== */
-#define CALLOC "calloc"
-#define MALLOC "malloc"
-#define FREE "free"
-#define REALLOC "realloc"
+#define STR_CALLOC "calloc"
+#define STR_MALLOC "malloc"
+#define STR_FREE "free"
+#define STR_REALLOC "realloc"
 
-VOID *track_start = (VOID*)0x060d000;
-VOID *track_end = (VOID*)0x0062e000;
+VOID *range_start = (VOID*)0x0060d000;
+VOID *range_end = (VOID*)0x0062e000;
 
-VOID RecordMemWrite(VOID *ip, VOID *addr, ADDRINT size)
+std::map<void *, unsigned char> mem_queue;
+MySQL *db;
+
+PIN_LOCK lock;
+
+void flush_queue(THREADID tid)
 {
-    if (!track_start || !track_end)
+    PIN_GetLock(&lock, tid+1);
+    if (mem_queue.size() == 0) {
+        PIN_ReleaseLock(&lock);
         return;
+    }
 
-    if ((ADDRINT)addr < (ADDRINT)track_start ||
-            (ADDRINT)addr > (ADDRINT)track_end)
-        return;
+    std::map<void*, unsigned char>::iterator it = mem_queue.begin();
+    for (; it != mem_queue.end(); it++) {
+        it->second = *(unsigned char*)it->first;
+    }
 
-    // TODO: implement
+    snapshot_t id = db->addSnapshot(snapshot_reason::MEM_WRITE);
+    db->addMemoryWrites(id, mem_queue);
+
+    mem_queue.clear();
+    PIN_ReleaseLock(&lock);
 }
 
-VOID* WrapperMalloc(CONTEXT *ctxt, AFUNPTR pf_malloc, size_t size)
+VOID RecordMemWrite(VOID *ip, VOID *addr, ADDRINT size, THREADID tid)
+{
+    if (!range_start|| !range_end)
+        return;
+
+    if ((ADDRINT)addr < (ADDRINT)range_start ||
+            (ADDRINT)addr > (ADDRINT)range_end)
+        return;
+
+    PIN_GetLock(&lock, tid+1);
+
+    for (ADDRINT i=0; i<size; i++) {
+        if (mem_queue.find((char*)addr+i) != mem_queue.end()) {
+            PIN_ReleaseLock(&lock);
+            flush_queue(tid);
+            PIN_GetLock(&lock, tid+1);
+        }
+
+        mem_queue[((char*)addr+i)] = 0;
+	printf("writing: %p <- 0x%02x\n", (char*)addr+i, *((unsigned char*)addr+i));
+    }
+
+    PIN_ReleaseLock(&lock);
+}
+
+VOID* WrapperMalloc(CONTEXT *ctxt, AFUNPTR pf_malloc, size_t size, THREADID tid)
 {
     void *res;
 
     CALL_APPLICATION_FUNCTION_PARAM param;
     memset((void*)&param, 0x00, sizeof(param));
+    param.native = 1;
+
+    flush_queue(tid);
 
     PIN_CallApplicationFunction(ctxt, PIN_ThreadId(),
             CALLINGSTD_DEFAULT, pf_malloc, &param,
             PIN_PARG(void *), &res, PIN_PARG(size_t), size,
             PIN_PARG_END());
 
+    PIN_GetLock(&lock, tid+1);
+    snapshot_t id = db->addSnapshot(snapshot_reason::MALLOC);
+    db->addReasonMalloc(id, res, size);
+    PIN_ReleaseLock(&lock);
+
+    flush_queue(tid);
+
     printf("malloc(%p) = %p\n", (void*)size, res);
 
     return res;
 }
 
-VOID WrapperFree(CONTEXT *ctxt, AFUNPTR pf_free, void *ptr)
+VOID WrapperFree(CONTEXT *ctxt, AFUNPTR pf_free, void *ptr, THREADID tid)
 {
     CALL_APPLICATION_FUNCTION_PARAM param;
     memset((void*)&param, 0x00, sizeof(param));
+    param.native = 1;
+
+    flush_queue(tid);
 
     PIN_CallApplicationFunction(ctxt, PIN_ThreadId(),
             CALLINGSTD_DEFAULT, pf_free, &param, PIN_PARG(void),
             PIN_PARG(void *), ptr,
             PIN_PARG_END());
 
+    PIN_GetLock(&lock, tid+1);
+    snapshot_t id = db->addSnapshot(snapshot_reason::FREE);
+    db->addReasonFree(id, ptr);
+    PIN_ReleaseLock(&lock);
+
+    flush_queue(tid);
+
     printf("free(%p)\n", ptr);
 }
 
-VOID* WrapperCalloc(CONTEXT *ctxt, AFUNPTR pf_calloc, size_t nmemb, size_t size)
+VOID* WrapperCalloc(CONTEXT *ctxt, AFUNPTR pf_calloc, size_t nmemb, size_t size,
+        THREADID tid)
 {
     void *res;
 
     CALL_APPLICATION_FUNCTION_PARAM param;
     memset((void*)&param, 0x00, sizeof(param));
+    param.native = 1;
+
+    flush_queue(tid);
 
     PIN_CallApplicationFunction(ctxt, PIN_ThreadId(),
             CALLINGSTD_DEFAULT, pf_calloc, &param,
@@ -79,23 +144,41 @@ VOID* WrapperCalloc(CONTEXT *ctxt, AFUNPTR pf_calloc, size_t nmemb, size_t size)
             PIN_PARG(size_t), size,
             PIN_PARG_END());
 
+    PIN_GetLock(&lock, tid+1);
+    snapshot_t id = db->addSnapshot(snapshot_reason::CALLOC);
+    db->addReasonCalloc(id, res, nmemb, size);
+    PIN_ReleaseLock(&lock);
+
+    flush_queue(tid);
+
     printf("calloc(%p, %p) = %p\n", (void*)nmemb, (void*)size, res);
 
     return res;
 }
 
-VOID* WrapperRealloc(CONTEXT *ctxt, AFUNPTR pf_realloc, void *ptr, size_t size)
+VOID* WrapperRealloc(CONTEXT *ctxt, AFUNPTR pf_realloc, void *ptr, size_t size,
+        THREADID tid)
 {
     void *res;
 
     CALL_APPLICATION_FUNCTION_PARAM param;
     memset((void*)&param, 0x00, sizeof(param));
+    param.native = 1;
+
+    flush_queue(tid);
 
     PIN_CallApplicationFunction(ctxt, PIN_ThreadId(),
             CALLINGSTD_DEFAULT, pf_realloc,
             &param, PIN_PARG(void *), &res,
             PIN_PARG(void *), ptr, PIN_PARG(size_t), size,
             PIN_PARG_END());
+
+    PIN_GetLock(&lock, tid+1);
+    snapshot_t id = db->addSnapshot(snapshot_reason::REALLOC);
+    db->addReasonRealloc(id, ptr, res, size);
+    PIN_ReleaseLock(&lock);
+
+    flush_queue(tid);
 
     printf("realloc(%p, %p) = %p\n", ptr, (void*)size, res);
 
@@ -113,6 +196,7 @@ VOID Instruction(INS ins, VOID *v)
                 (AFUNPTR) RecordMemWrite,
                 IARG_INST_PTR, IARG_MEMORYWRITE_EA,
                 IARG_MEMORYWRITE_SIZE,
+                IARG_THREAD_ID,
                 IARG_END);
     }
 }
@@ -120,37 +204,39 @@ VOID Instruction(INS ins, VOID *v)
 VOID ImageLoad(IMG img, VOID *v)
 {
     // replace malloc singature
-    RTN rtn_malloc = RTN_FindByName(img, MALLOC);
+    RTN rtn_malloc = RTN_FindByName(img, STR_MALLOC);
     if (RTN_Valid(rtn_malloc)) {
         PROTO proto_malloc = PROTO_Allocate(PIN_PARG(void*),
-                CALLINGSTD_DEFAULT, MALLOC, PIN_PARG(size_t),
+                CALLINGSTD_DEFAULT, STR_MALLOC, PIN_PARG(size_t),
                 PIN_PARG_END());
 
         RTN_ReplaceSignature(rtn_malloc, AFUNPTR(WrapperMalloc),
                 IARG_PROTOTYPE, proto_malloc, IARG_CONTEXT,
                 IARG_ORIG_FUNCPTR,
                 IARG_FUNCARG_ENTRYPOINT_VALUE, 0,
+                IARG_THREAD_ID,
                 IARG_END);
     }
 
     // replace free singature
-    RTN rtn_free = RTN_FindByName(img, FREE);
+    RTN rtn_free = RTN_FindByName(img, STR_FREE);
     if (RTN_Valid(rtn_free)) {
         PROTO proto_free = PROTO_Allocate(PIN_PARG(void),
-                CALLINGSTD_DEFAULT, FREE, PIN_PARG(void *),
+                CALLINGSTD_DEFAULT, STR_FREE, PIN_PARG(void *),
                 PIN_PARG_END());
 
         RTN_ReplaceSignature(rtn_free, AFUNPTR(WrapperFree),
                 IARG_PROTOTYPE, proto_free, IARG_CONTEXT,
                 IARG_ORIG_FUNCPTR,
                 IARG_FUNCARG_ENTRYPOINT_VALUE, 0,
+                IARG_THREAD_ID,
                 IARG_END);
     }
     // replace calloc singature
-    RTN rtn_calloc = RTN_FindByName(img, CALLOC);
+    RTN rtn_calloc = RTN_FindByName(img, STR_CALLOC);
     if (RTN_Valid(rtn_calloc)) {
         PROTO proto_calloc = PROTO_Allocate(PIN_PARG(void*),
-                CALLINGSTD_DEFAULT, CALLOC, PIN_PARG(size_t),
+                CALLINGSTD_DEFAULT, STR_CALLOC, PIN_PARG(size_t),
                 PIN_PARG(size_t),
                 PIN_PARG_END());
 
@@ -159,14 +245,15 @@ VOID ImageLoad(IMG img, VOID *v)
                 IARG_ORIG_FUNCPTR,
                 IARG_FUNCARG_ENTRYPOINT_VALUE, 0,
                 IARG_FUNCARG_ENTRYPOINT_VALUE, 1,
+                IARG_THREAD_ID,
                 IARG_END);
     }
 
     // replace realloc singature
-    RTN rtn_realloc = RTN_FindByName(img, REALLOC);
+    RTN rtn_realloc = RTN_FindByName(img, STR_REALLOC);
     if (RTN_Valid(rtn_realloc)) {
         PROTO proto_realloc = PROTO_Allocate(PIN_PARG(void*),
-                CALLINGSTD_DEFAULT, REALLOC, PIN_PARG(void *),
+                CALLINGSTD_DEFAULT, STR_REALLOC, PIN_PARG(void *),
                 PIN_PARG(size_t),
                 PIN_PARG_END());
 
@@ -175,6 +262,7 @@ VOID ImageLoad(IMG img, VOID *v)
                 IARG_CONTEXT, IARG_ORIG_FUNCPTR,
                 IARG_FUNCARG_ENTRYPOINT_VALUE, 0,
                 IARG_FUNCARG_ENTRYPOINT_VALUE, 1,
+                IARG_THREAD_ID,
                 IARG_END);
     }
 }
@@ -202,11 +290,15 @@ INT32 Usage()
 
 int main(int argc, char *argv[])
 {
+    PIN_InitLock(&lock);
+
     // Initialize pin & symbol manager
     PIN_InitSymbols();
     if (PIN_Init(argc,argv)) {
         return Usage();
     }
+
+    db = new MySQL("localhost", "root", "root", "ihv");
 
     // Register Image to be called to instrument functions.
     IMG_AddInstrumentFunction(ImageLoad, 0);
